eeee163bab2d1256d487403964cfe8950a621066
Lan Phan <lanc_phan@hotmail.com>
2017-09-19 13:37:09 -0400

    replaced the films into an array and added the if/elsif statement

--- film-01.rb
+++ film-01.rb
@@ -10,26 +10,73 @@
 # If they say yes to only comedies, recommend the comedy.
 # If they answer no to all three, recommend a good book instead.
 
+############ A LIST OF FILMS #############
 # Documentary Film
-film01 = "The Cove"
-film02 = "Man on Wire"
-film03 = "An Inconvenient Truth"
-film04 = "Grizzly Man"
-film05 = "March of the Penguins"
+doc_film = [
+  "The Cove",
+  "Man on Wire",
+  "An Inconvenient Truth",
+  "Grizzly Man", "March of the Penguins"
+]
 
 # Drama Film
-film06 = "The Godfather"
-film07 = "The Shawshank Redemption"
-film08 = "Schlindler's List"
-film09 = "Pulp Fiction"
-film10 = "12 Angry Men"
+drama_film = [
+  "The Godfather",
+  "The Shawshank Redemption",
+  "Schlindler's List",
+  "Pulp Fiction",
+  "12 Angry Men"
+]
 
 # Comedy Film
-film11 = "Rush Hour 2"
-film12 = "The Hangover"
-film13 = "Bridesmaids"
-film14 = "Mean Girls"
-film15 = "Austin Powers: International Man of Mystery"
+comedy_film = [
+  "Rush Hour 2",
+  "The Hangover",
+  "Bridesmaids",
+  "Mean Girls",
+  "Austin Powers: International Man of Mystery"
+]
 
-puts "Which of the three genres do you like to watch: documentaries, dramas, or comedies?"
-answer = gets.chomp
+# Dramedy Film
+dramedy_film = [
+  "Eternal Sunshine of the Spotless Mind",
+  "500 Days of Summer",
+  "Little Miss Sunshine",
+  "Silver Linings Playbook",
+  "The Breakfast Club"
+]
+
+############## QUESTIONS ###############
+puts "I have three genres of films for you to choose. Do you like documentaries?"
+answer01 = gets.chomp
+
+puts "Do you like drama?"
+answer02 = gets.chomp
+
+puts "Do you like comedy?"
+answer03 = gets.chomp
+
+############# METHODS ##############
+
+# documentaries only
+if answer01 == "yes" && answer02 == "no" && answer03 == "no"
+  puts "Here are five films I would recommend you to watch: #{doc_film}"
+
+# Dramedy only
+elsif answer01 == "no" && answer02 == "yes" && answer03 == "yes"
+  puts "Here are the five films I would recommend you to watch: #{dramedy_film}"
+
+# Drama only
+elsif answer01 == "no" && answer02 == "yes" && answer03 = "no"
+  puts "Here are five films I would recommend you to watch: #{drama_film}"
+
+# Comedy Only
+elsif answer01 == "no" && answer02 == "no" && answer03 = "yes"
+  puts "Here are the five films I would recommend you to watch: #{comedy_film}"
+
+# ALL FILMS
+elsif answer01 == "yes" && answer02 == "yes" && answer03 = "yes"
+  puts "#{doc_film}, #{drama_film},#{dramedy_film}, #{comedy_film}"
+
+else
+  puts "If you don't like these genres, then I would recommend reading a novel like Stephen King's IT"
